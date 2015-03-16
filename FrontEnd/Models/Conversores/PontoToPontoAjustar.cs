829bbc1204f607c9209404e493945cd0615237dc
Marlonvss <marlonvss@gmail.com>
2015-03-15 21:08:02 -0300

    Alteracoes do Ajuste

---
+++ FrontEnd/Models/Conversores/PontoToPontoAjustar.cs
@@ -0,0 +1,26 @@
+﻿using Dominio.Model;
+using Seedwork.Conversores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FrontEnd.Models.Conversores
+{
+    public class PontoToPontoAjustar : ConversorBase<Ponto, PontoEditar>
+    {
+        public override void AplicarValores(Ponto origem, PontoEditar destino)
+        {
+            destino.DataAjuste = origem.DataAjuste;
+            destino.MotivoAjuste = origem.MotivoAjuste;
+            destino.AjusteAprovado = origem.AjusteAprovado;
+        }
+
+        public override void AplicarValores(PontoEditar origem, Ponto destino)
+        {
+            destino.DataAjuste = origem.DataAjuste;
+            destino.MotivoAjuste = origem.MotivoAjuste;
+            destino.AjusteAprovado = origem.AjusteAprovado;
+        }
+    }
+}
---
+++ FrontEnd/Models/PontoEditar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace FrontEnd.Models
+{
+    public class PontoEditar : EntityModel
+    {
+
+        [Description("Data Marcada")]
+        public DateTime DataDaMarcacao { get; set; }
+
+        [Description("Data do Ajuste")]
+        public DateTime DataAjuste { get; set; }
+
+        [Description("Motivo do Ajuste")]
+        public String MotivoAjuste { get; set; }
+
+        [Description("Status")]
+        public Boolean AjusteAprovado { get; set; }
+
+    }
+}
