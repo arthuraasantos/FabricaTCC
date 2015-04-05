00f998a98b1a39950ab49f0b033049e51cd8ccf6
MarlonVSS <marlonvss@gmail.com>
2015-04-05 11:19:57 -0300

    Nova ferramenta de ajuste de ponto; Novo Layout de Consulta de Horarios; Nova visao da Dashboard; Nova ferramenta de aprovacao/rejeicao de ajuste de ponto;

---
+++ Seedwork/Const/Enumerados.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seedwork.Const
+{
+    public abstract class EnumPonto
+    {
+        public enum Aprovacao { Nada, Aprovado, Rejeitado };
+    }
+
+}
