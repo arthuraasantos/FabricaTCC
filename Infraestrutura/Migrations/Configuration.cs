e0d22a12d551e36e02c6c5aeca5602495fad0084
arthuraasantos <arthuraasantos@hotmail.com>
2015-04-14 10:46:17 -0300

    Acerto dados padroes iniciais sistema

--- Infraestrutura/Migrations/Configuration.cs
+++ Infraestrutura/Migrations/Configuration.cs
@@ -19,17 +19,38 @@ namespace Infraestrutura.Migrations
 
         protected override void Seed(Infraestrutura.MyContext context)
         {
+            #region 'Perfil de Acesso'
+            
             var perfil = new PerfilDeAcesso()
             {
                 Id = Guid.Parse("09c15773-d1a5-4daa-a3b3-e8c6741d63fd"),
-                Descricao = "Gerente"
+                Descricao = "Administrador"
             };
-            context.Set<PerfilDeAcesso>().Add(perfil);
 
+            var perfilRH = new PerfilDeAcesso()
+            {
+                Id = Guid.Parse("4036b285-e7fb-4ac0-aac4-5196fde05dd8"),
+                Descricao = "Gerente/RH"
+            };
+
+            var perfilFuncionario = new PerfilDeAcesso()
+            {
+                Id = Guid.Parse("3acc5625-0663-47c2-bb4d-e507ee5b004f"),
+                Descricao = "FuncionarioComum"
+            };
+
+            context.Set<PerfilDeAcesso>().Add(perfil);
+            context.Set<PerfilDeAcesso>().Add(perfilRH);
+            context.Set<PerfilDeAcesso>().Add(perfilFuncionario);
+
+            #endregion
+
+            #region 'Empresas'
+            
             var empresa = new Empresa()
             {
                 Id = Guid.Parse("e6a1d40d-05bf-4a81-b1fd-72893c07a23e"),
-                RazaoSocial = "Craque do Pão e Pizzaria",
+                RazaoSocial = "Empresa Administrador",
                 Bairro = "Campo Grande",
                 Bloqueado = "N",
                 Cep = "23070420",
@@ -42,10 +63,31 @@ namespace Infraestrutura.Migrations
                 Logradouro = "Estrada do Campinho"
                 
             };
+
+            var empresaRH = new Empresa()
+            {
+                Id = Guid.Parse("bbd66ce1-9feb-4eb5-8611-7a1a8b59f3c2"),
+                RazaoSocial = "Empresa Rh",
+                Bairro = "Centro",
+                Bloqueado = "N",
+                Cep = "23070400",
+                Cidade = "Rio de Janeiro",
+                Cnpj = "87335085000147",
+                Estado = "RJ",
+                NomeFantasia = "RH Consultoria",
+                NumeroEndereco = 1330,
+                Pais = "Brasil",
+                Logradouro = "Estrada do Lavradio"
+
+            };
+
             context.Set<Empresa>().Add(empresa);
+            context.Set<Empresa>().Add(empresaRH);
+
+            #endregion
+
+            #region 'Funcionários'
             
-
-
             var funcionario = new Funcionario()
             {
                 Id = Guid.Parse("69d2fad2-9ffe-4f2d-ad85-3ff4b277f805"),
@@ -58,7 +100,7 @@ namespace Infraestrutura.Migrations
 
             var funcionarioMarlon = new Funcionario()
             {
-                Id = Guid.NewGuid(),
+                Id = Guid.Parse("26341447-4897-4d49-851d-25890888e463"),
                 Nome = "Marlon",
                 Email = "marlonvss@gmail.com",
                 Senha = "admin",
@@ -68,17 +110,41 @@ namespace Infraestrutura.Migrations
 
             var funcionarioCharles = new Funcionario()
             {
-                Id = Guid.NewGuid(),
+                Id = Guid.Parse("73ee557e-bd03-469d-8ed4-034266e1e82f"),
                 Nome = "Charles",
                 Email = "charles.info@ymail.com",
                 Senha = "admin",
                 Empresa = empresa,
                 PerfilDeAcesso = perfil
+            };  
+
+            var funcionarioTeste = new Funcionario()
+            {
+                Id = Guid.Parse("4eb58c13-5077-4e24-ba4c-8d0173ed3942"),
+                Nome = "Usuário Teste",
+                Email = "administrador@fabricatcc.com",
+                Senha = "admin",
+                Empresa = empresaRH,
+                PerfilDeAcesso = perfilFuncionario
+            };
+
+            var funcionarioRH = new Funcionario()
+            {
+                Id = Guid.Parse("01e9ba15-272a-4871-8377-6d2f4e621a67"),
+                Nome = "Usuário Teste RH",
+                Email = "rh@fabricatcc.com",
+                Senha = "admin",
+                Empresa = empresaRH,
+                PerfilDeAcesso = perfilRH
             };            
 
             context.Set<Funcionario>().Add(funcionario);
             context.Set<Funcionario>().Add(funcionarioMarlon);
             context.Set<Funcionario>().Add(funcionarioCharles);
+            context.Set<Funcionario>().Add(funcionarioTeste);
+            context.Set<Funcionario>().Add(funcionarioRH);
+
+            #endregion
         }
     }
 }
