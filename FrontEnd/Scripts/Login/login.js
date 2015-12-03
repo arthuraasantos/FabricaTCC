7f3f0c08a204ae3be1cb56cbbe2fdf5aa63a41cf
arthuraasantos <arthuraasantos@hotmail.com>
2015-12-03 00:33:14 -0200

    Alteração de nomes de mensagens para inglês Início de retirada da Session da página de login

---
+++ FrontEnd/Scripts/Login/login.js
@@ -0,0 +1,56 @@
+﻿$(function () {
+    $(".btn-success").click(function () {
+         $.ajax({
+            url: "/Login/LoginValidate",
+            data: {
+                email: $("input[type=email][name=Email]").val(),
+                password: $("input[type=password][name=Password]").val()
+            },
+            type: "GET",
+            datatype: "json",
+            async: true ,
+            sucess: function (response) {
+                alert("chegou aqui");
+                if (response.IsValid) {
+                    // Faz login
+                    doLogin();
+                }
+                else
+                {
+                    // exibir div de erro
+                    if (response.Type == "Warning") {
+                        ShowWarning(response.Message)
+                    }
+                    else if (data.Type == "Error") {
+                        ShowDanger(response.Message)
+                    }
+                }
+            }, 
+            error: function(response){
+                alert("teste error"+ response);
+            }
+        });
+    });
+});
+
+function doLogin(){
+    alert("chegou no dologin");
+
+    $.ajax({
+            url: "/Login/Autenticar",
+            data: {
+                email: $("input[type=email][name=Email]").val(),
+                password: $("input[type=password][name=Password]").val()
+            },
+            type: "GET",
+            datatype: "json",
+            contenttype: "application/json; charset=utf-8",
+        }).fail(function(response){
+            // erro ao logar
+            ShowDanger(response.Message);
+    });
+    
+    alert("terminou sem dar erro");
+}
+
+
