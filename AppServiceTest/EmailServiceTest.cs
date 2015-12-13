5e7271f759aa84acdb23abfbfe790086e767e8c0
arthuraasantos <arthuraasantos@hotmail.com>
2015-12-13 18:18:55 -0200

    Testes unitários do serviço de email

---
+++ AppServiceTest/EmailServiceTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TCCPontoEletronico.AppService.Entity;
+
+namespace AppServiceTest
+{
+    /// <summary>
+    /// Testes do serviço de e-mail. Testes relacionados ao serviço de e-mail
+    /// </summary>
+    [TestClass]
+    public class EmailServiceTest
+    {
+        [TestMethod]
+        public void EmptyEmailTest()
+        {
+            string email = string.Empty;
+
+            var response = EmailService.IsValid(email);
+
+            Assert.IsFalse(response);
+        }
+
+        [TestMethod]
+        public void WhiteSpaceEmailTest()
+        {
+            string email = "  ";
+
+            var response = EmailService.IsValid(email);
+
+            Assert.IsFalse(response);
+        }
+
+        [TestMethod]
+        public void EmailValidTest()
+        {
+            string email = "contoso@pontonation.com";
+
+            var isValid = EmailService.IsValid(email);
+
+            Assert.IsTrue(isValid);
+        }
+    }
+}
