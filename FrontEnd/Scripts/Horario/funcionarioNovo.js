041893f170553dc54b73284f8500587b51b16184
Arthur Augusto <arthuraasantos@gmail.com>
2016-07-31 22:00:21 -0300

    Correção editar e listar horario de expediente

--- FrontEnd/Scripts/Horario/funcionarioNovo.js
+++ FrontEnd/Scripts/Horario/funcionarioNovo.js
@@ -9,34 +9,14 @@
         type: "GET",
         datatype: "json",
         async: true,
-        //contenttype: "application/json; charset=utf-8",
         success: function (lista) {
-            //alert(lista.length);
-            $("#IdHorarioDeExpediente").empty();
-            $.each(lista, function (i, item) {
-                $("#IdHorarioDeExpediente").append('<option value=" ' + item.Id + ' ">' + item.Text + '</option>');
-            })
+            if (lista.length > 0) {
+                $("#IdHorarioDeExpediente").empty();
+                $.each(lista, function (i, item) {
+                    $("#IdHorarioDeExpediente").append('<option value=" ' + item.Value + ' ">' + item.Text + '</option>');
+                });
+            }
         }
     })
 };
-//    .done(function (msg) {
-//        //var comboHorarios = document.getElementById('IdHorarioDeExpediente');
-//        //comboHorarios.innerHTML = "";
-//        var options = "";
-//        msg.forEach(function (item) {
-//            alert('teste');
-//            options += '<option value="' + item.Id + '">' + item.Descricao + '</option>';
-
-//        });
-//        $("#IdHorariosDeExpediente").html(options); 
-//    })
-//};
-    //success: function (lista) {
-    //    alert(lista.length);
-    //    $("#IdHorariosDeExpediente").empty();
-    //    for (var i = 0; i < lista.length; i++) {
-    //        $("#IdHorariosDeExpediente").append("<option>" + lista[i] + "</option>");
-    //    }
-    //},
-//});
 
